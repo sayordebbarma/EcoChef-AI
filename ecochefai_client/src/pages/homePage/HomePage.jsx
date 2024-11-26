f0b060d9ac8454a3b54fa73df70b14062029d843
sayordebbarma <sayorofficial8543@gmail.com>
2024-11-26 16:33:09 +0530

    setting up the recipes section

--- ecochefai_client/src/pages/homePage/HomePage.jsx
+++ ecochefai_client/src/pages/homePage/HomePage.jsx
@@ -2,13 +2,17 @@ import React from 'react';
 import HeroSection from './HeroSection';
 import AboutUs from './AboutUs';
 import MeetTheTeam from './MeetTheTeam';
+import Navbar from '../../components/navbar/Navbar';
+import Footer from '../../components/footer/Footer';
 
 const HomePage = () => {
   return (
     <>
+      <Navbar />
       <HeroSection />
       <AboutUs />
       <MeetTheTeam />
+      <Footer />
     </>
   );
 };
