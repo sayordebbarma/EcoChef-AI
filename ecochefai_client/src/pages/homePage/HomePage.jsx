abd6dca1c251d2787398f3202d205c4c07d9ef7e
sayordebbarma <sayorofficial8543@gmail.com>
2024-11-26 16:03:14 +0530

    Introducing the team members

--- ecochefai_client/src/pages/homePage/HomePage.jsx
+++ ecochefai_client/src/pages/homePage/HomePage.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import HeroSection from './HeroSection';
 import AboutUs from './AboutUs';
+import MeetTheTeam from './MeetTheTeam';
 
 const HomePage = () => {
   return (
     <>
       <HeroSection />
       <AboutUs />
+      <MeetTheTeam />
     </>
   );
 };
